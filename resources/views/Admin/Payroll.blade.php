21d9cbe76d9c7330e722573b8fe4fd2e33cb4abd
jihansaharani <saharanijihan1@gmail.com>
2023-06-08 16:05:13 +0700

    /startpayroll dan /employeeadmin

--- resources/views/Admin/Payroll.blade.php
+++ resources/views/Admin/Payroll.blade.php
@@ -859,7 +859,7 @@
                     </a>
 
                 </li>
-                <li><a href="/Payroll" class="ai-icon hover-red" aria-expanded="false">
+                <li><a href="/StartPayroll" class="ai-icon hover-red" aria-expanded="false">
                         <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960"
                             width="48">
                             <path
