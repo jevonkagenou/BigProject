5c418e585aa94b43bbe3fc95fae3ccdc48061e2c
naokichiro <naokikagenou@gmail.com>
2023-06-08 08:43:49 +0700

    Validation Hover & Checked Red

--- resources/views/Admin/Validation.blade.php
+++ resources/views/Admin/Validation.blade.php
@@ -1,4 +1,16 @@
-@include('PartialAdmin.SidebarNavbar')
+@include('PartialAdmin.SidebarNavbar')\
+
+    <style>
+       .btn-check:checked+.btn-outline-danger,
+        .btn-
+        e+.btn-outline-danger,
+        .btn-outline-danger:active,
+        .btn-outline-danger.active,
+        .btn-outline-danger.dropdown-toggle.show {
+            color: white !important;
+        }
+    </style>
+
         <div class="content-body">
             <div class="container-fluid">
                 <div class="row">
@@ -12,20 +24,20 @@
                                 <div class="default-tab">
                                     <ul class="nav nav-tabs" role="tablist">
                                         <li class="nav-item">
-                                            <a class="nav-link active" data-bs-toggle="tab" href="#presensi">Presensi</a>
+                                            <a class="nav-link active" data-bs-toggle="tab" style="color: #f72b50;" href="#presensi">Presensi</a>
                                         </li>
                                         <li class="nav-item">
-                                            <a class="nav-link" data-bs-toggle="tab" href="#lembur">Lembur</a>
+                                            <a class="nav-link" data-bs-toggle="tab" style="color: #f72b50;" href="#lembur">Lembur</a>
                                         </li>
                                     </ul>
                                     <div class="tab-content">
                                         <div class="tab-pane fade show active" id="lembur" role="tabpanel">
                                             <div class="pt-4">
                                                 <h6>Apakah selalu diperlukan approval saat lembur?</h6>
-                                                <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                                                <div class="btn-group white-color" role="group" aria-label="Basic radio toggle button group">
 													<input type="radio" class="btn-check" name="btnradio1a" id="btnradio1" checked>
 													<label class="btn btn-outline-danger btn-xs" for="btnradio1">Ya</label>
-													
+
 													<input type="radio" class="btn-check" name="btnradio1a" id="btnradio2">
 													<label class="btn btn-outline-danger btn-xs" for="btnradio2">Tidak</label>
 												</div>
@@ -108,7 +120,7 @@
            Support ticket button end
         ***********************************-->
 
-        
+
     </div>
     <!--**********************************
         Main wrapper end
