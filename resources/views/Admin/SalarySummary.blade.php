d961c3c565e4a7c233292d8a9b78a7ba9989923e
naokichiro <naokikagenou@gmail.com>
2023-06-09 14:34:40 +0700

    mm-active Sidebar

--- resources/views/Admin/SalarySummary.blade.php
+++ resources/views/Admin/SalarySummary.blade.php
@@ -1,3 +1,6 @@
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
     <meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -45,6 +48,11 @@
         }
          .dlabnav .metismenu a{
             color: #9FA4A6;
+        }
+        [data-sidebar-style="full"]
+        [data-layout=":vertical"]
+        .dlabnav .metismenu{
+            padding: 0 30px !important;
         }
 	</style>
 		<div class="nav-header ">
@@ -756,8 +764,8 @@
             Sidebar start
         ***********************************-->
         <div class="dlabnav">
-            <div class="dlabnav-scroll">
-                <ul class="metismenu" id="menu">
+            <div class="dlabnav-scroll mm-active ps ps--active-y">
+                <ul class="metismenu mm-show" id="menu">
                     <li class="dropdown header-profile">
                         <a class="nav-link" style="cursor: default;" role="button" data-bs-toggle="dropdown">
                             <img src="{{ asset('images/profile/pic1.jpg') }}" width="20" alt="" />
@@ -784,9 +792,9 @@
                         </a>
 
                     </li>
-                    <li><a class=" ai-icon hover-red" href="AdminReport" aria-expanded="false">
+                    <li class="mm-active"><a class=" ai-icon hover-red mm-active" href="AdminReport" aria-expanded="false">
                         <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M349-250h262q12.75 0 21.375-8.675 8.625-8.676 8.625-21.5 0-12.825-8.625-21.325T611-310H349q-12.75 0-21.375 8.675-8.625 8.676-8.625 21.5 0 12.825 8.625 21.325T349-250Zm0-170h262q12.75 0 21.375-8.675 8.625-8.676 8.625-21.5 0-12.825-8.625-21.325T611-480H349q-12.75 0-21.375 8.675-8.625 8.676-8.625 21.5 0 12.825 8.625 21.325T349-420ZM220-80q-24 0-42-18t-18-42v-680q0-24 18-42t42-18h336q12.444 0 23.722 5T599-862l183 183q8 8 13 19.278 5 11.278 5 23.722v496q0 24-18 42t-42 18H220Zm331-584v-156H220v680h520v-494H581q-12.75 0-21.375-8.625T551-664ZM220-820v186-186 680-680Z"/></svg>
-                            <span class="nav-text ">Laporan</span>
+                            <span class="nav-text">Laporan</span>
                         </a>
 
                     </li>
@@ -814,9 +822,9 @@
                             <span class="nav-text ">Payroll</span>
                         </a>
                     </li>
-                    <li><a class=" ai-icon hover-red" href="/Setting " aria-expanded="false">
+                    <li><a class=" ai-icon hover-red" href="/Setting " aria-expanded="true">
                         <svg xmlns="http://www.w3.org/2000/svg" height="48" viewBox="0 -960 960 960" width="48"><path d="M546-80H414q-11 0-19.5-7T384-105l-16-101q-19-7-40-19t-37-25l-93 43q-11 5-22 1.5T159-220L93-337q-6-10-3-21t12-18l86-63q-2-9-2.5-20.5T185-480q0-9 .5-20.5T188-521l-86-63q-9-7-12-18t3-21l66-117q6-11 17-14.5t22 1.5l93 43q16-13 37-25t40-18l16-102q2-11 10.5-18t19.5-7h132q11 0 19.5 7t10.5 18l16 101q19 7 40.5 18.5T669-710l93-43q11-5 22-1.5t17 14.5l66 116q6 10 3.5 21.5T858-584l-86 61q2 10 2.5 21.5t.5 21.5q0 10-.5 21t-2.5 21l86 62q9 7 12 18t-3 21l-66 117q-6 11-17 14.5t-22-1.5l-93-43q-16 13-36.5 25.5T592-206l-16 101q-2 11-10.5 18T546-80Zm-66-270q54 0 92-38t38-92q0-54-38-92t-92-38q-54 0-92 38t-38 92q0 54 38 92t92 38Zm0-60q-29 0-49.5-20.5T410-480q0-29 20.5-49.5T480-550q29 0 49.5 20.5T550-480q0 29-20.5 49.5T480-410Zm0-70Zm-44 340h88l14-112q33-8 62.5-25t53.5-41l106 46 40-72-94-69q4-17 6.5-33.5T715-480q0-17-2-33.5t-7-33.5l94-69-40-72-106 46q-23-26-52-43.5T538-708l-14-112h-88l-14 112q-34 7-63.5 24T306-642l-106-46-40 72 94 69q-4 17-6.5 33.5T245-480q0 17 2.5 33.5T254-413l-94 69 40 72 106-46q24 24 53.5 41t62.5 25l14 112Z"/></svg>
-                            <span class="nav-text ">Pengaturan</span>
+                            <span class="nav-text">Pengaturan</span>
                         </a>
 
                     </li>
