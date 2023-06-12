d851084e91d8beb10d0e12c9382836ec003504ff
naokichiro <naokikagenou@gmail.com>
2023-06-12 09:35:05 +0700

    Sedikit Perubahan Saja pt2

--- resources/views/DashboardEmployee/DashboardEmployee.blade.php
+++ resources/views/DashboardEmployee/DashboardEmployee.blade.php
@@ -43,6 +43,9 @@
         .red-hover:hover{
             color:#EE3232 !important;
         }
+        .announcement{
+            color: #EE3232 !important;
+        }
     </style>
 
 
@@ -102,7 +105,7 @@
                         <div class="justify-content-end">
                             <ul class="navbar-nav">
                                 <li class="nav-item dropdown notification_dropdown mx-2">
-                                    <a class="nav-link bell bell-link" href="#">
+                                    <a class="nav-link bell bell-link" href="javascript:void(0)">
                                      <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
                                             <path fill-rule="evenodd" clip-rule="evenodd" d="M27 7.88883C27 5.18897 24.6717 3 21.8 3C17.4723 3 10.5277 3 6.2 3C3.3283 3 1 5.18897 1 7.88883V23.7776C1 24.2726 1.31721 24.7174 1.80211 24.9069C2.28831 25.0963 2.8473 24.9912 3.2191 24.6417C3.2191 24.6417 5.74629 22.2657 7.27769 20.8272C7.76519 20.3688 8.42561 20.1109 9.11591 20.1109H21.8C24.6717 20.1109 27 17.922 27 15.2221V7.88883ZM24.4 7.88883C24.4 6.53951 23.2365 5.44441 21.8 5.44441C17.4723 5.44441 10.5277 5.44441 6.2 5.44441C4.7648 5.44441 3.6 6.53951 3.6 7.88883V20.8272L5.4382 19.0989C6.4132 18.1823 7.73661 17.6665 9.11591 17.6665H21.8C23.2365 17.6665 24.4 16.5726 24.4 15.2221V7.88883ZM7.5 15.2221H17.9C18.6176 15.2221 19.2 14.6745 19.2 13.9999C19.2 13.3252 18.6176 12.7777 17.9 12.7777H7.5C6.7824 12.7777 6.2 13.3252 6.2 13.9999C6.2 14.6745 6.7824 15.2221 7.5 15.2221ZM7.5 10.3333H20.5C21.2176 10.3333 21.8 9.7857 21.8 9.11104C21.8 8.43638 21.2176 7.88883 20.5 7.88883H7.5C6.7824 7.88883 6.2 8.43638 6.2 9.11104C6.2 9.7857 6.7824 10.3333 7.5 10.3333Z" fill="black"/>
                                         </svg>
@@ -401,7 +404,7 @@
 																<h5 class="mt-1 mb-1">Pengumuman</h5>
 																<hr>
 																<p class="mb-0">Hari ini di karenakan perusahaan...</p>
-																<a href="" class="text-red d-flex justify-content-end">Lihat</a>
+																<a href="javascript:void(0)" class="text-red d-flex justify-content-end nav-link bell bell-link announcement red-hover">Lihat</a>
 
 															</div>
 														</div>
@@ -415,7 +418,7 @@
 																<h5 class="mt-1 mb-1">Pengumuman</h5>
 																<hr>
 																<p class="mb-0">Hari ini di karenakan perusahaan...</p>
-																<a href="" class="text-red d-flex justify-content-end">Lihat</a>
+																<a href="javascript:void(0)" class="text-red d-flex justify-content-end nav-link bell bell-link announcement red-hover">Lihat</a>
 															</div>
 														</div>
 													</div>
