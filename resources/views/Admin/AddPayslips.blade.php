6f135eaff196074864cd5390cc282b724cb78e99
nanda <anandaharfiana@gmail.com>
2023-06-14 11:17:22 +0700

    href payrol pada karyawan

--- resources/views/Admin/AddPayslips.blade.php
+++ resources/views/Admin/AddPayslips.blade.php
@@ -1097,7 +1097,7 @@
         ***********************************-->
 <div class="footer">
     <div class="copyright">
-        <p>Copyright © Designed &amp; Developed by <a href="https://dexignlab.com/" target="_blank">DexignLab</a> 2022
+        <p>Copyright © Designed &amp; Developed by <a href="https://dexignlab.com/" target="_blank">Kalopsia</a> 2022
         </p>
     </div>
 </div>
