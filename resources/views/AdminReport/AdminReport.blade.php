53005aebe8f60249c3c888a0de22c7b39fb1be04
jihansaharani <saharanijihan1@gmail.com>
2023-06-19 11:30:37 +0700

    Update Admin Report. dropdown

--- resources/views/AdminReport/AdminReport.blade.php
+++ resources/views/AdminReport/AdminReport.blade.php
@@ -1,119 +1,101 @@
 @include('PartialAdmin.SidebarNavbar')
 
-        <div class="content-body">
-            <!-- row -->
-			<div class="container-fluid">
-				<div class="row">
-					<style>
-						a {
-						  text-decoration: none;
-						}
-					</style>
-						<div class="row">
-							<div class="col-md-4" name="ellipsis-vertical-outline;">
-								<a href="/SalarySummary">
-									<div class="card progress-card">
-									  <p>
-										<div class="img_cont">
-										  <center><img src="https://i.postimg.cc/tRKhxKnV/ringkasangaji.png" class="rounded-circle user_img" alt="width=30%" /></center>
-										  <span class="online_icon"></span>
-										</div>
-										<div class="card-body d-flex; justify-content: center;">
-										  <div class="me-auto">
-											<center><h4 class="card-title">Ringkasan Gaji</h4></center>
-										  </div>
-										</div>
-									</div>
-								</a>
-							</div>
-							<div class="col-md-4"  >
-								<a href="/SalaryPaymentReport">
-									<div class="card progress-card">
-									  <p>
-										<div class="img_cont">
-										  <center><img src="https://i.postimg.cc/XNRVjhkV/laporan2.png" class="rounded-circle user_img" alt="width=30%" /></center>
-										  <span class="online_icon"></span>
-										</div>
-										<div class="card-body d-flex; justify-content: center;">
-										  <div class="me-auto">
-											<center><h4 class="card-title">Pembayaran Gaji</h4></center>
-										  </div>
-										</div>
-									</div>
-								</a>
-							</div>
-							<div class="col-md-4"  >
-								<a href="/SummaryofComponentSalary">
-									<div class="card progress-card">
-									  <p>
-										<div class="img_cont">
-										  <center><img src="https://i.postimg.cc/sxxGFJLS/komponen.png" class="rounded-circle user_img" alt="width=30%" /></center>
-										  <span class="online_icon"></span>
-										</div>
-										<div class="card-body d-flex; justify-content: center;">
-										  <div class="me-auto">
-											<center><h4 class="card-title">Ringkasan Gaji Perkomponen</h4></center>
-										  </div>
-										</div>
-									</div>
-								</a>
-							</div>
-						</div>
-					</div>
-				</div>
+<div class="content-body">
+    <div class="container-fluid align-items-center">
+        <div class="row">
+            <div class="col-xl-12 col-xxl-4">
+                <a href="/SalarySummary">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-xl-12">
+                                    <div class="d-flex align-items-center justify-content-center">
+                                        <img class="img-responsive" src="https://i.postimg.cc/XNRVjhkV/laporan2.png" alt="">
+                                    </div>
+                                    <div class="text-center mt-4">
+                                        <strong>
+                                            <h4>Ringkasan Gaji	</h4>
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
             </div>
+            <div class="col-xl-12 col-xxl-4">
+                <a href="/SalaryPaymentReport">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-xl-12">
+                                    <div class="d-flex align-items-center justify-content-center">
+                                        <img class="img-responsive" src="images/foto5.png" alt="">
+                                    </div>
+                                    <div class="text-center mt-4">
+                                        <strong>
+                                            <h4>Pembayaran Gaji</h4>
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+			<div class="col-xl-12 col-xxl-4">
+                <a href="/SummaryofComponentSalary">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-xl-12">
+                                    <div class="d-flex align-items-center justify-content-center">
+                                        <img class="img-responsive" src="images/foto5.png" alt="">
+                                    </div>
+                                    <div class="text-center mt-4">
+                                        <strong>
+                                            <h4>Ringkasan Gaji Perkomponen</h4>
+                                        </strong>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+           
         </div>
-        <!--**********************************
-            Content body end
-        ***********************************-->
+    </div>
+</div>
 
+<div class="footer">
+    <div class="copyright">
+        <p>Copyright © Designed &amp; Developed by <a href="https://dexignlab.com/"
+                target="_blank">Kalopsia</a> 2022</p>
+    </div>
+</div>
+<script src="{{ asset('vendor/global/global.min.js') }}"></script>
+<script src="{{ asset('vendor/chart.js/Chart.bundle.min.js') }}"></script>
+<script src="{{ asset('vendor/jquery-nice-select/js/jquery.nice-select.min.js') }}"></script>
 
+<!-- Apex Chart -->
+<script src="{{ asset('vendor/apexchart/apexchart.js') }}"></script>
+<script src="{{ asset('vendor/nouislider/nouislider.min.js') }}"></script>
+<script src="{{ asset('vendor/wnumb/wNumb.js') }}"></script>
 
-        <!--**********************************
-            Footer start
-        ***********************************-->
-        
-        <!--**********************************
-            Footer end
-        ***********************************-->
+<!-- Dashboard 1 -->
+<script src="{{ asset('js/dashboard/dashboard-1.js') }}"></script>
 
+<script src="{{ asset('js/custom.min.js') }}"></script>
+<script src="{{ asset('js/dlabnav-init.js') }}"></script>
+<script src="{{ asset('js/demo.js') }}"></script>
+<script src="{{ asset('js/styleSwitcher.js') }}"></script>
 
+<!-- Apex Chart -->
+<script src="{{ asset('vendor/apexchart/apexchart.js') }}"></script>
 
+<script src="{{ asset('vendor/jquery-nice-select/js/jquery.nice-select.min.js') }}"></script>
 
-	</div>
-    <!--**********************************
-        Main wrapper end
-    ***********************************-->
-
-    <!--**********************************
-        Scripts
-    ***********************************-->
-	<div class="footer">
-
-		<div class="copyright">
-			<p>Copyright © Designed &amp; Developed by <a href="https://dexignlab.com/" target="_blank">Kalopsia</a> 2022</p>
-		</div>
-	</div>
-    <!-- Required vendors -->
-	<script src="{{ asset('vendor/global/global.min.js') }}"></script>
-	<script src="vendor/chart.js/Chart.bundle.min.js"></script>
-	<script src="vendor/jquery-nice-select/js/jquery.nice-select.min.js"></script>
-
-	<!-- Apex Chart -->
-	<script src="vendor/apexchart/apexchart.js"></script>
-	<script src="vendor/nouislider/nouislider.min.js"></script>
-	<script src="vendor/wnumb/wNumb.js"></script>
-
-	<!-- Dashboard 1 -->
-	<script src="js/dashboard/dashboard-1.js"></script>
-
-    <script src="js/custom.min.js"></script>
-	<script src="js/dlabnav-init.js"></script>
-	<script src="js/demo.js"></script>
-    <script src="js/styleSwitcher.js"></script>
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
-
-</body>
-
-<!-- Mirrored from dompet.dexignlab.com/xhtml/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Thu, 11 May 2023 08:53:06 GMT -->
-</html>
+<!-- Chart ChartJS plugin files -->
+<script src="{{ asset('vendor/chart.js/Chart.bundle.min.js') }}"></script>
+<script src="{{ asset('js/plugins-init/chartjs-init.js') }}"></script>
