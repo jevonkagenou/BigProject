3332693c6d2f10d8ecdf210a6fab414405616fb8
naokichiro <naokikagenou@gmail.com>
2023-06-21 11:20:50 +0700

    Database Employee

---
+++ database/migrations/2023_06_21_015715_create_data_employees_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('data_employees', function (Blueprint $table) {
+            $table->id();
+            $table->string('longname');
+            $table->string('place_birth');
+            $table->date('date');
+            $table->string('gender');
+            $table->string('marry');
+            $table->string('blood_group');
+            $table->string('region');
+            $table->string('email');
+            $table->integer('numberphone');
+            $table->string('address');
+            $table->string('last_study');
+            $table->string('educational_institution');
+            $table->string('study_program');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('data_employees');
+    }
+};
