e74722492a12d7b0f4b46e21814f48c6348ec221
jihansaharani <saharanijihan1@gmail.com>
2023-06-22 09:46:37 +0700

    Migration tok

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,11 +13,14 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('name');
+            $table->bigInteger('notelp');
             $table->string('password');
+            $table->string('foto') ->nullable();
+            $table->string('role');
             $table->rememberToken();
+            $table->timestamp('email_verified_at')->nullable();
             $table->timestamps();
         });
     }
