206ce4ec08c626f642708282afd71f1e609ae997
jevonkagenou <kichironomercy@gmail.com>
2023-07-07 15:07:31 +0700

    Model Payroll

---
+++ app/Models/Payroll.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Payroll extends Model
+{
+    use HasFactory;
+    protected $guarded = [];
+
+    public function DataEmployee()
+    {
+        return $this->belongsTo(DataEmployee::class, 'data_employee_id');
+    }
+}
