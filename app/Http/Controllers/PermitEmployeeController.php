e18c6a017469912e8b54b4db5c77df4f26e6e553
jevonkagenou <kichironomercy@gmail.com>
2023-07-09 13:44:44 +0700

    Permit Employee

--- app/Http/Controllers/PermitEmployeeController.php
+++ app/Http/Controllers/PermitEmployeeController.php
@@ -16,23 +16,23 @@ class PermitEmployeeController extends Controller
             'pilihan' => 'required',
             'description' => 'required'
         ]);
-    
+
         $date1 = Carbon::parse($request->input('submission_date'));
         $date2 = Carbon::parse($request->input('date_leave'));
-    
+
         $total_day = $date2->diffInDays($date1);
-        
+
         $data = PermitEmployee::create([
-            'user_id' => $request->user_id,
-            'submission_date' => $request->submission_date,
-            'date_leave' => $request->date_leave,
-            'tipe' => $request->pilihan,
+            'user_id' => auth()->user()->id,
+            'submission_date' => $request->input('submission_date'),
+            'date_leave' => $request->input('date_leave'),
+            'tipe' => $request->input('pilihan'),
             'total_day' => $total_day,
-            'description' => $request->description
+            'description' => $request->input('description')
         ]);
-    
-        // Lakukan tindakan lain yang diperlukan setelah data izin ditambahkan
-    
+
         return response()->json(['message' => 'Permit added successfully', 'data' => $data], 201);
     }
+
+
 }
