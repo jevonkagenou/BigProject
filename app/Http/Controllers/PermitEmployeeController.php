7ab3b584737d2037aa49c0858a6ec5c09e32482c
jevonkagenou <kichironomercy@gmail.com>
2023-07-10 09:32:30 +0700

    PermitEmployee

--- app/Http/Controllers/PermitEmployeeController.php
+++ app/Http/Controllers/PermitEmployeeController.php
@@ -9,14 +9,12 @@ use Carbon\Carbon;
 class PermitEmployeeController extends Controller
 {
     public function Add_Permit(Request $request){
-        if (!auth()->check()) {
-            return redirect('/Login');
-        }
+
 
         $request->validate([
             'submission_date' => 'required',
             'date_leave' => 'required',
-            'pilihan' => 'required',
+            'tipe' => 'required',
             'description' => 'required'
         ]);
 
@@ -26,10 +24,10 @@ class PermitEmployeeController extends Controller
         $total_day = $date2->diffInDays($date1);
 
         $data = PermitEmployee::create([
-            'user_id' => auth()->user()->id,
+            'user_id' => Auth()->id(),
             'submission_date' => $request->input('submission_date'),
             'date_leave' => $request->input('date_leave'),
-            'tipe' => $request->input('pilihan'),
+            'tipe' => $request->input('tipe'),
             'total_day' => $total_day,
             'description' => $request->input('description')
         ]);
